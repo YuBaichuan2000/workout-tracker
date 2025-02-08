a45a3b7ab1cdde5e316b4812922ac0b883b09669
George Yu <george@think.digital>
2025-02-08 14:43:21 +1100

    Finished workout controllers and routes

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -45,3 +45,48 @@ export const createWorkout = async (req, res) => {
     }
 
 }
+
+export const deleteWorkout = async (req, res) => {
+    try {
+        const {id} = req.params;
+        // check if object id is valid
+        if (!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({error: 'No such workout'});
+        }
+
+        const workout = await Workout.findOneAndDelete({_id: id});
+
+        if (!workout) {
+            return res.status(404).json({error: 'No such workout'});
+        }
+
+        res.status(200).json(workout)
+
+    } catch (e) {
+        res.status(400).json({error: e.message});
+    };
+};
+
+export const updateWorkout = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        // check if object id is valid
+        if (!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({error: 'No such workout'});
+        }
+
+        const workout = await Workout.findOneAndUpdate({_id: id}, {
+            ...req.body
+        })
+
+        if (!workout) {
+            return res.status(404).json({error: 'No such workout'});
+        }
+
+        res.status(200).json(workout);
+    } catch (e) {
+        res.status(400).json({error: e.message});
+    };
+
+};
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createWorkout, getSingleWorkout, getWorkouts }  from '../controllers/workoutController.js';
+import { createWorkout, getSingleWorkout, getWorkouts, updateWorkout, deleteWorkout }  from '../controllers/workoutController.js';
 
 
 const router = express.Router();
@@ -10,13 +10,9 @@ router.get('/:id', getSingleWorkout);
 
 router.post('/', createWorkout);
 
-router.delete('/:id', (req, res) => {
-    res.json({msg: 'DELETE a workout'});
-});
+router.delete('/:id', deleteWorkout);
 
-router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a workout'});
-});
+router.patch('/:id', updateWorkout);
 
 
 export default router;
