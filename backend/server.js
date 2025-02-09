4587cdd46c661d58e1a2736a2b086cd15a8a2e41
George Yu <george@think.digital>
2025-02-09 20:10:32 +1100

    Fixed cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://workout-tracker-f15p.onrender.com/'
+  }));
+  
 app.use(express.json());
 
 app.use('/api/workouts', workoutRoutes);
