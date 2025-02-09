087f7e9e12c236590d985a2423e0b4ee72fc0e1c
George Yu <george@think.digital>
2025-02-09 20:26:59 +1100

    Fixed frontend urls

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -8,7 +8,7 @@ const WorkoutDetails = ( {workout} ) => {
     const { dispatch } = useWorkoutsContext();
 
     const handleClick = async () => {
-        const response = await fetch('https://workout-tracker-f15p.onrender.com//api/workouts/'+workout._id, {method: 'DELETE'});
+        const response = await fetch('https://workout-tracker-f15p.onrender.com/api/workouts/'+workout._id, {method: 'DELETE'});
         const json = await response.json();
         
         if (response.ok) {
