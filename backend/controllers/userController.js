0877d944a6c80417fd186bb1e31e9f86fb2eab3d
George Yu <george@think.digital>
2025-02-11 17:03:11 +1100

    Added jsonwebtoken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 import User from  '../models/userModels.js';
+import jwt from 'jsonwebtoken';
 
+const createToken = (_id) => {
+    return jwt.sign({_id: _id}, process.env.SECRET, {expiresIn: '1d'});
+}
 
 // login user
 export const loginUser = async (req, res) => {
@@ -13,7 +17,12 @@ export const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password);
-        res.status(200).json({email, user});
+
+        // create token
+        const token = createToken(user._id);
+
+        res.status(200).json({email, token});
+
     } catch (e) {
         res.status(400).json({error: e.message});
     }
